d65517682781f38b3f3ffd82488c1f693be00672
Asanga Abeyaratne <asaabey@gmail.com>
2019-06-18 07:06:32 +0930

    template engine added to rman_pckg with mods

--- tkc-select-synopsis-report.sql
+++ tkc-select-synopsis-report.sql
@@ -3,31 +3,10 @@ declare
     composition clob;
     eid         integer;
 
-function get_composition_by_eid(eid_in int,nlc_id varchar2) return clob
-as
-composition clob;    
-begin
-with cte1 as (
-SELECT eid, att, dt,rman_pckg.map_to_tmplt(t0.valc,tmp.templatehtml) as body,tmp.placementid
-FROM(
-    SELECT
-        eid,dt,att,valc,src,ROW_NUMBER() OVER (PARTITION BY eid,att ORDER BY dt) AS rn
-    FROM eadvx
-) t0
-JOIN rman_rpt_templates tmp on tmp.ruleblockid=t0.src
-WHERE t0.rn=1
-and eid=eid_in)
-select LISTAGG(body, '') WITHIN GROUP(ORDER BY placementid) into composition
-FROM cte1
-GROUP BY eid;
-
-return composition;
-
-end get_composition_by_eid;
 
 begin
     eid:=6053;
-    composition:=get_composition_by_eid(eid,'neph001');
+    composition:=rman_pckg.get_composition_by_eid(eid,'neph001');
     DBMS_OUTPUT.put_line('EID:' || eid);
 
     DBMS_OUTPUT.put_line(composition);
