82a7b2ceb70718bc8d0fb29f5ae304b267db4343
Asanga Abeyaratne <asabe@prod.main.ntgov>
2019-06-22 21:56:41 +0930

    tkc cvra

--- tkc-execute-ruleblock.sql
+++ tkc-execute-ruleblock.sql
@@ -7,7 +7,7 @@ DECLARE
 BEGIN 
 --    rman_pckg.execute_ruleblock('cd_dm_2',1,1);
     
-    rman_pckg.execute_ruleblock('ckd_2_1',1,1);
+    rman_pckg.execute_ruleblock('cvra_1_1',1,0);
 --    rman_pckg.execute_ruleblock('cd_htn_2');
     
 --    rman_pckg.execute_ruleblock('ckd_cause_2_1');
