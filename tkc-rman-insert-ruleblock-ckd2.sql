4b3eba1d2f7df67966de93bdb15fc71100467347
asaabey <asaabey@gmail.com>
2020-09-07 11:58:00 +0930

    fixes

--- tkc-rman-insert-ruleblock-ckd2.sql
+++ tkc-rman-insert-ruleblock-ckd2.sql
@@ -1154,7 +1154,7 @@ BEGIN
             }
         );
         
-        avf => eadv.[caresys_3450901,caresys_3451200,caresys_3451800].dt.max();
+        avf => eadv.[caresys_3450901,caresys_3451200,caresys_3451800,icpc_k99049].dt.max();
         
         
         avf_has : { avf!?  =>1},{=>0};
