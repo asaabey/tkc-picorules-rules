cd5e23842e77610addd97d0abdf1e57f33359224
Asanga Abeyaratne <asaabey@gmail.com>
2022-03-19 17:07:20 +0930

    fixed location logic

--- tkc-rman-insert-ruleblock-rrt-hd-loc.sql
+++ tkc-rman-insert-ruleblock-rrt-hd-loc.sql
@@ -32,20 +32,38 @@ BEGIN
             }
         );
 
-        loc_ca_gap_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count(0).where(loc=111710100010132 and dt >= sysdate-30);
-        loc_ca_fd_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count(0).where(loc=111710100010122 and dt >= sysdate-30);
-        loc_ca_tch_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count(0).where(loc=111710500012022 and dt >= sysdate-30);
-        loc_ca_other_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count(0).where(loc IN (111710111460001, 111710111460051, 111710410202051) and dt >= sysdate-30);
-        loc_te_nru_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count(0).where(loc=111720600013032 and dt >= sysdate-30);
-        loc_te_7ad_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count(0).where(loc=111720600015062 and dt >= sysdate-30);
-        loc_te_kdh_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count(0).where(loc IN (111720900016042, 111720900016052) and dt >= sysdate-30);
-        loc_te_pdu_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count(0).where(loc=111720600005012 and dt >= sysdate-30);
-        loc_te_tiwi_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count(0).where(loc=111721200017012 and dt >= sysdate-30);
-        loc_te_other_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count(0).where(loc IN (111720600014022, 111720600013012) and dt >= sysdate-30);
+        loc_ca_gap_1m_n => eadv.[caresys_1310000].dt.distinct_count()
+                                .where(loc=111711800010132 and dt >= sysdate-30);
+        loc_ca_fd_1m_n => eadv.[caresys_1310000].dt.distinct_count()
+                                .where(loc=111711800010122 and dt >= sysdate-30);
+        loc_ca_tch_1m_n => eadv.[caresys_1310000].dt.distinct_count()
+                                .where(loc=111711800010122 and dt >= sysdate-30);
+        loc_ca_all_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count()
+                                .where(substr(loc,1,5)=`11171` and dt >= sysdate-30);
+                                
+        loc_ca_other_1m_n : {coalesce(loc_ca_all_1m_n,0) > coalesce(loc_ca_gap_1m_n,loc_ca_fd_1m_n, loc_ca_fd_1m_n, loc_ca_tch_1m_n, loc_ca_all_1m_n,0) => loc_ca_all_1m_n };
+        
+        loc_te_nru_1m_n => eadv.[caresys_1310000].dt.distinct_count()
+                                .where(loc=111721600013032 and dt >= sysdate-30);
+        loc_te_7ad_1m_n => eadv.[caresys_1310000].dt.distinct_count()
+                                .where(loc in(111721600015062,111721600006002) and dt >= sysdate-30);
+        loc_te_kdh_1m_n => eadv.[caresys_1310000].dt.distinct_count()
+                                .where(loc IN (111721500016042, 111721500008032) and dt >= sysdate-30);
+        loc_te_pdu_1m_n => eadv.[caresys_1310000].dt.distinct_count()
+                                .where(loc=111721600014022 and dt >= sysdate-30);
+        loc_te_tiwi_1m_n => eadv.[caresys_1310000].dt.distinct_count()
+                                .where(loc=111721600017012 and dt >= sysdate-30);
+        loc_te_all_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count()
+                                .where(substr(loc,1,5)=`11172` and dt >= sysdate-30);
+        loc_te_other_1m_n : {coalesce(loc_te_all_1m_n,0) > coalesce(loc_te_nru_1m_n,loc_te_7ad_1m_n, loc_te_kdh_1m_n, loc_te_pdu_1m_n, loc_te_tiwi_1m_n,0) => loc_te_all_1m_n };
 
-        loc_undef_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count(0).where(loc NOT IN (111710100010132, 111710100010122, 111710500012022, 111710111460001, 111710111460051, 111710410202051, 111720600014022, 111720600013012, 111721200017012, 111720600005012, 111720900016042, 111720900016052, 111720600015062, 111720600013032) and dt >= sysdate-30);
-
-        loc_all_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count(0).where(dt >= sysdate-30);
+        loc_prplhouse_1m_n => eadv.[mbs_13105].dt.distinct_count()
+                                .where(substr(loc,1,5)=`11571`and dt >= sysdate-30); 
+        loc_miwatj_1m_n => eadv.[mbs_13105].dt.distinct_count()
+                                .where(substr(loc,1,5)=`13472`and dt >= sysdate-30); 
+                                            
+        loc_all_1m_n => eadv.[caresys_1310000,caresys_1310004,icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].dt.distinct_count()
+                                .where(dt >= sysdate-30);
 
         [[rb_id]] => eadv.[caresys_1310000,caresys_1310004, icpc_u59001,icpc_u59008,icd_z49_1,mbs_13105].loc.last();
 
