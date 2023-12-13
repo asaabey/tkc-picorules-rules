686214361c1baa8d2f65c852b812321a49ddcb2b
Asanga Abeyaratne <asaabey@gmail.com>
2023-12-13 19:07:47 +0930

    Add files via upload

--- tkc-rman-insert-ruleblock-dmg-loc-origin.sql
+++ tkc-rman-insert-ruleblock-dmg-loc-origin.sql
@@ -11,14 +11,15 @@ DECLARE
 BEGIN
     
    
-       
-        -- BEGINNING OF RULEBLOCK --
+
+     -- BEGINNING OF RULEBLOCK --
 
     rb.blockid:='dmg_loc_origin';
 
     DELETE FROM rman_ruleblocks WHERE blockid=rb.blockid;
     
     rb.picoruleblock:='
+    
         /* Algorithm to assess demographics origin*/
         
         #define_ruleblock(dmg_loc_origin,
@@ -33,7 +34,7 @@ BEGIN
         
         /* Uses 715 assumption */
         
-        loc_first => eadv.[mbs_715, mbs_13105].loc.first();
+        loc_first => eadv.[mbs%].loc.first();
         
         loc_code : {loc_first!? => substr(loc_first,4)};
         
@@ -48,7 +49,7 @@ BEGIN
         #define_attribute(loc_code,
         {
                 label:"Origin PHC Location code",
-                type:1,
+                type:1002,
                 is_reportable:1
         });
         
@@ -71,6 +72,43 @@ BEGIN
                 type:1,
                 is_reportable:1
         });
+        
+        
+        
+                
+    ';
+    rb.picoruleblock := replace(rb.picoruleblock,'[[rb_id]]',rb.blockid);
+    
+    rb.picoruleblock:=rman_pckg.sanitise_clob(rb.picoruleblock);
+
+    INSERT INTO rman_ruleblocks(blockid,picoruleblock) VALUES(rb.blockid,rb.picoruleblock);
+    
+    
+    -- END OF RULEBLOCK 
+    
+    -- BEGINNING OF RULEBLOCK --
+
+    rb.blockid:='dmg_loc2';
+
+    DELETE FROM rman_ruleblocks WHERE blockid=rb.blockid;
+    
+    rb.picoruleblock:='
+    
+        /* Algorithm to assess demographics -method 2 */
+        
+        #define_ruleblock(dmg_loc2, {
+                description: "Algorithm to assess demographics -method 2",
+                is_active:0});
+        
+        my_freq_remote_clinic => eadv.[icd_%,icpc_%,lab_%,rxnc_%,obs_%,mbs_%].loc.stats_mode().where(dt > sysdate - 750  and substr(loc,15,1)= 1 and substr(loc,4,12) not in (`710500019011`,`711800001001`,`711800001011`,`711800001021`,`711800001031`,`711800001041`,`711800001051`,`711811460001`,`711811460051`,`721500018021`,`721511495001`,`721511495011`,`721511495021`,`721590139011`,`721600002001`,`721600002091`,`721600002101`,`721600002111`,`721600002121`,`721600002131`,`721610857151`,`721610878161`,`721611471011`,`721611479051`));
+      
+        dmg_loc2 : { my_freq_remote_clinic!? =>1 },{=>0};
+      
+      
+ );
+        
+        
+                
     ';
     rb.picoruleblock := replace(rb.picoruleblock,'[[rb_id]]',rb.blockid);
     
